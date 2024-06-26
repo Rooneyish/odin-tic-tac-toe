d3785ebc861b296e23fb1abd6ffa7d253d0c9293
Rooneyish <ilustraterp@gmail.com>
2024-06-26 16:50:56 +0545

    5th commit, working on js not finished yetgit add .!

--- script.js
+++ script.js
@@ -1,53 +1,80 @@
 const Gameboard = (function () {
   let gameboard = {
     gameboard: ["", "", "", "", "", "", "", "", ""],
+    currentPlayer:'X',
+    player1:'',
+    player2:'',
+    squares:[],
+
     init: function () {
       this.cacheDOM();
       this.render();
       this.bindEvents();
+      console.log(this.gameboard)
     },
 
     cacheDOM: function () {
       this.board = document.querySelector("#gameboard");
       this.startBtn = document.querySelector("#start");
       this.restartBtn = document.querySelector("#restart");
-      this.player1 = document.querySelector("#player1");
-      this.player2 = document.querySelector("#player2");
+      this.player1Name = document.querySelector("#player1");
+      this.player2Name = document.querySelector("#player2");
       this.message= document.querySelector(".message");
     },
 
     bindEvents: function () {
       this.startBtn.addEventListener("click", this.startGame.bind(this));
       this.restartBtn.addEventListener("click", this.restartGame.bind(this));
+      this.board.addEventListener('click', this.handleClickTurn.bind(this))
     },
 
     render: function () {
-      let gboard = {
-        gameboard: this.gameboard,
-      };
       this.board.innerHTML = "";
-      gboard.gameboard.forEach((index) => {
+      this.squares=[];
+      this.gameboard.forEach((value,index) => {
         const square = document.createElement("div");
         square.id = `square-${index}`;
-        square.setAttribute("value", index);
+        square.setAttribute("data-index", index);
+        square.textContent=value;
         this.board.appendChild(square);
+        this.squares.push(square);
       });
     },
+
     clearTextField: function () {
-      this.player1.value = "";
-      this.player2.value = "";
+      this.player1Name.value= "";
+      this.player2Name.value= "";
     },
 
     startGame: function (event) {
       event.preventDefault();
+      this.player1=this.player1Name.value;
+      this.player2=this.player2Name.value;
+      this.message.innerHTML=`Lets play the game!!! ${this.player1}'s turn.`
       this.clearTextField();
-      this.message.innerHTML=`Lets play the game!!! ${this.player1.value}'s turn.`
     },
 
     restartGame: function (event) {
       event.preventDefault();
-      console.log("hello");
+      this.currentPlayer='X'
+      this.gameboard=["","","","","","","","",""]
+      this.message.innerHTML='Welcome to Tic-Tac-Toe!!'
+      this.render();
     },
+
+    handleClickTurn:function(event){
+      const index=event.target.dataset.index;
+      if(this.gameboard[index]===''){
+        this.gameboard[index]=this.currentPlayer;
+        this.currentPlayer=this.currentPlayer==="X"?"0":'X';
+        this.render()
+      }
+    },
+
+    checkWinner: function(){
+
+    }
+
   };
   gameboard.init();
 })();
