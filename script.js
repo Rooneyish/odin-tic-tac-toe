6982e02aa16f6cef5200aefb7656aed6a87c9cd7
Rooneyish <ilustraterp@gmail.com>
2024-06-26 09:12:22 +0545

    4th commit, updated js, html, and css, js using modular programming

--- script.js
+++ script.js
@@ -1,21 +1,53 @@
-const Gameboard = (function(){
-    let gameboard=["","","","","","","","","",]
+const Gameboard = (function () {
+  let gameboard = {
+    gameboard: ["", "", "", "", "", "", "", "", ""],
+    init: function () {
+      this.cacheDOM();
+      this.render();
+      this.bindEvents();
+    },
 
+    cacheDOM: function () {
+      this.board = document.querySelector("#gameboard");
+      this.startBtn = document.querySelector("#start");
+      this.restartBtn = document.querySelector("#restart");
+      this.player1 = document.querySelector("#player1");
+      this.player2 = document.querySelector("#player2");
+      this.message= document.querySelector(".message");
+    },
 
-    const display=()=>{
-        const board=document.querySelector('#gameboard');
-        board.innerHTML=""
-        gameboard.forEach((value,index)=>{
-            const square= document.createElement('div');
-            square.id=`square-${index}`;
-            square.setAttribute('value',index);
-            board.appendChild(square)
-        }) 
-        console.log('hello');
-    }
-    return {
-        display,
-    }
+    bindEvents: function () {
+      this.startBtn.addEventListener("click", this.startGame.bind(this));
+      this.restartBtn.addEventListener("click", this.restartGame.bind(this));
+    },
+
+    render: function () {
+      let gboard = {
+        gameboard: this.gameboard,
+      };
+      this.board.innerHTML = "";
+      gboard.gameboard.forEach((index) => {
+        const square = document.createElement("div");
+        square.id = `square-${index}`;
+        square.setAttribute("value", index);
+        this.board.appendChild(square);
+      });
+    },
+    clearTextField: function () {
+      this.player1.value = "";
+      this.player2.value = "";
+    },
+
+    startGame: function (event) {
+      event.preventDefault();
+      this.clearTextField();
+      this.message.innerHTML=`Lets play the game!!! ${this.player1.value}'s turn.`
+    },
+
+    restartGame: function (event) {
+      event.preventDefault();
+      console.log("hello");
+    },
+  };
+  gameboard.init();
 })();
-
-Gameboard.display()
